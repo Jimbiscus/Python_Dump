61353d81e5af7b8f26b0d7e37458db0b95c15270
Jimbiscus <Hasborae@gmail.com>
2020-10-13 23:15:56 +0200

    update

--- ClassesAndObject.py
+++ ClassesAndObject.py
@@ -9,14 +9,28 @@ class Aventurier:
 
     def fullname(self):
        return f"{self.name} the {self.job}"
+    def levelUp(self):
+        self.hp = int(self.hp * 1.05)
+        self.atk = int(self.atk + 3)
+        self.res = int(self.res + 1)
+        self.gold = int(self.gold + 100)
+    def showStats(self):
+        print(self.name + " the " + self.job)
+        print("hp : " + str(self.hp))
+        print("atk : " + str(self.atk))
+        print("def : " + str(self.res))
+        print("gold : " + str(self.gold))
+
 
 Av1 = Aventurier("Jim", "Wizard", 40, 3, 3, 0)
 Av2 = Aventurier("Jolan", "Necromancer", 30, 5, 4, 0)
 
-print(Av2.name)
+Av2.showStats()
+print("VERSUS")
+Av1.showStats()
+Av1.levelUp()
+Av1.showStats()
 
-print(Av1.name)
-print(Av2.fullname())
 
-print(Av1.fullname())
-print(Aventurier.fullname(Av1))
+Av1.hp = 10000000
+Av1.showStats()
