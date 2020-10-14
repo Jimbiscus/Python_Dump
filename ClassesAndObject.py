b5b899ebcee4da192ec1db6539ee8d0f3671ec73
Jimbiscus <Hasborae@gmail.com>
2020-10-14 09:41:25 +0200

    update on classes

--- ClassesAndObject.py
+++ ClassesAndObject.py
@@ -15,22 +15,18 @@ class Aventurier:
         self.res = int(self.res + 1)
         self.gold = int(self.gold + 100)
     def showStats(self):
-        print(self.name + " the " + self.job)
+        print(self.fullname())
         print("hp : " + str(self.hp))
         print("atk : " + str(self.atk))
         print("def : " + str(self.res))
         print("gold : " + str(self.gold))
 
 
-Av1 = Aventurier("Jim", "Wizard", 40, 3, 3, 0)
-Av2 = Aventurier("Jolan", "Necromancer", 30, 5, 4, 0)
+Av1 = Aventurier("Thomas", "Wizard", 40, 3, 3, 0)
+Av2 = Aventurier("Bastien", "Barbarian", 30, 5, 4, 0)
 
 Av2.showStats()
 print("VERSUS")
-Av1.showStats()
 Av1.levelUp()
 Av1.showStats()
 
-
-Av1.hp = 10000000
-Av1.showStats()
