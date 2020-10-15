b52fea7b2157a6f8ae451692876b94e4f3cb2ea6
Jimbiscus <Hasborae@gmail.com>
2020-10-15 16:05:47 +0200

    working oxo2

--- Aventurer's Adventure/oxo2.py
+++ Aventurer's Adventure/oxo2.py
@@ -8,9 +8,32 @@ board = [
     [".", ".", ".", ".", "."],
     [".", ".", ".", ".", "."]]
 
-
-
 # --- Functions ---
+def write():
+    f = open("oxo.txt","r")
+    g = open("oxo_results.txt", "a")
+    for line in f.readlines():
+        g.write(line)
+
+def play():
+
+    # code qui permet à l'utilisateur de choisir une position valide en x et en y
+    column = input("Quelle colonne? --->(0-4): ")
+    column = int(column)
+    
+    row = input("Quelle ligne? \/ (0-4): ")
+    row = int(row)
+
+    while row < 0 or row > 4 or column < 0 or column > 4 or give(column, row) != '.':
+        print(f"La coordonnée {column}, {row} est invalide!")
+        column = input("Quelle colonne? (0-4): ")
+        column = int(column)
+        
+        row = input("Quelle ligne? (0-4): ")
+        row = int(row)
+
+    return column, row
+
 
 def display():
     txt = "  01234\n"
@@ -23,12 +46,14 @@ def display():
         txt += "\n"
     print(txt)
 
+
 def give(x, y):
+    if x < 0 or x > 4 or y < 0 or y > 4:
+        return None
     return board[y][x]
 
-def place(carac):
-    x = int(input("x : "))
-    y = int(input("y : "))
+
+def place(carac, x, y):
     board[y][x] = carac
 
 
@@ -36,6 +61,7 @@ def init():
     for n in range(len(board)):
         del board[0]
         board.append(['.', '.', '.', '.', '.'])
+
 
 def is_full():
     full = True
@@ -46,23 +72,66 @@ def is_full():
                 full = False
 
     return full
-def is_xxx():
-    for row in board:
-        pass
-# --- here start the script ---
 
-x = 0
+
+def check_victory(x, y):
+
+    player = give(x, y)
+
+    if give(x + 1, y) == player:
+        if give(x + 2, y) == player:
+            return True
+        if give(x - 1, y) == player:
+            return True
+    if give(x - 1, y) == player and give(x - 2, y) == player:
+        return True
+
+    if give(x, y + 1) == player:
+        if give(x, y + 2) == player:
+            return True
+        if give(x, y - 1) == player:
+            return True
+    if give(x, y - 1) == player and give(x, y - 2) == player:
+        return True
+
+
+    return False
+
+# --- here start the script ---
+display()
+
+turn = 0
+print("-= Init Round =-")
+x, y = play()
 while True:
-    if x == 0: 
-        print("Player One")
-        place(PLAYER_1)
-        display()
-        x = 1
-        is_full()
-    elif x == 1:
+    
+    while turn == 0:            
         
-        print("Player Two")
-        place(PLAYER_2)
+        
+        place(PLAYER_1, x, y)
         display()
-        x = 0
-        is_full()
+        write()
+        turn = 1
+
+    while turn == 1 and check_victory(x, y) == False and is_full() == False:
+        print("-= Player Two =-")
+        player = "Player Two"
+        x, y = play()
+        place(PLAYER_2, x, y)
+        display()
+        check_victory(x, y)
+        write()
+        turn = 2
+    while turn == 2 and check_victory(x, y) == False and is_full() == False:
+        print("-= Player One =- ")
+        player = "Player One"
+        x, y = play()
+        place(PLAYER_1, x, y)
+        display()
+        check_victory(x, y)
+        write()
+        turn = 1
+    if check_victory(x, y) == True:
+        print(f"{player} Wins !")
+        break
+
